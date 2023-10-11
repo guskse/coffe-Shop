f6c0ffb9ab01b0c5c543c9298cf2c269cd92b64f
gustavo <guskse@gmail.com>
2023-10-11 08:59:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//mobile menu
+const burgerIcon = document.getElementById("burger");
+const navbarMenu = document.getElementById("nav-links");
+
+burgerIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
+});
+
+//tabs
+const tabs = document.querySelectorAll(".tabs li");
+const tabContentBoxes = document.querySelectorAll("#tab-content > div");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => {
+        
+      item.classList.remove("is-active");
+
+      tab.classList.add("is-active");
+
+      const target = tab.dataset.target;
+
+      tabContentBoxes.forEach((box) => {
+        if (box.getAttribute("id") === target) {
+          box.classList.remove("is-hidden");
+        } else {
+          box.classList.add("is-hidden");
+        }
+      });
+    });
+  });
+});
